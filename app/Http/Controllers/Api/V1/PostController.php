dd263ff9466505b2d236fe5423a1b240c3b2c06b
codeurient <codeurient@gmail.com>
2026-01-18 13:36:37 +0400

    StorePostRequest

--- app/Http/Controllers/Api/V1/PostController.php
+++ app/Http/Controllers/Api/V1/PostController.php
@@ -5,49 +5,44 @@ namespace App\Http\Controllers\Api\V1;
 use App\Models\Post;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StorePostRequest;
 
 class PostController extends Controller
 {
     public function index()
     {
-        return 'index_v1';
+        $posts = Post::all();
+        return $posts;
     }
 
-    public function store(Request $request)
+    public function store(StorePostRequest $request)
+    {
+        $data = $request->validated();
+
+        $data['author_id'] = 2;
+        $post = Post::create($data);
+        return response()->json($post, 201);
+    }
+
+    public function show(Post $post)
+    {
+        return $post;
+    }
+
+    public function update(Request $request, Post $post)
     {
         $data = $request->validate([
             'title' => 'required|string|min:2',
             'body' => ['required', 'string', 'min:2']
         ]);
 
-        $data['author_id'] = 1;
-
-        $post = Post::create($data);
-
-        return response()->json($post, 201);
+        $post->update($data);
+        return $post; 
     }
 
-    public function show(string $id)
+    public function destroy(Post $post)
     {
-        return response()->json([
-            [
-                'message' => 'test',
-                'data' => [
-                    'id' => 1,
-                    'title' => 'Test',
-                    'body' => 'Post body',
-                ]
-            ]
-        ])->header('Test', 'Zura');
-    }
-
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    public function destroy(string $id)
-    {
-        //
+        $post->delete();
+        return response()->noContent();
     }
 }
