e24619a2061ef8765e2562066ed3b5b17551f15b
codeurient <codeurient@gmail.com>
2026-01-20 17:44:48 +0400

    ImageGenerationResource

--- app/Http/Controllers/Api/V1/PromptGenerationController.php
+++ app/Http/Controllers/Api/V1/PromptGenerationController.php
@@ -2,10 +2,41 @@
 
 namespace App\Http\Controllers\Api\V1;
 
-use App\Http\Controllers\Controller;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use App\Services\OpenAiService;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\GeneratePromptRequest;
+use App\Http\Resources\ImageGenerationResource;
 
 class PromptGenerationController extends Controller
 {
-    //
+
+    public function __construct(private OpenAiService $openAiService) {}
+
+    public function store(GeneratePromptRequest $request)
+    {
+        $user            = $request->user();
+        $image           = $request->file('image');
+
+        $originalName    = $image->getClientOriginalName();
+        $sanitizedName   = preg_replace('/[^a-zA-Z0-9._-]/', '_', pathinfo($originalName, PATHINFO_FILENAME)); // something.png -> something.png
+        $extension       = $image->getClientOriginalExtension();
+        $safeFilename    = $sanitizedName . '_' . Str::random(32) . '.' . $extension;
+
+        $imagePath       = $image->storeAs('uploads/images', $safeFilename, 'public');
+
+        $generatedPrompt = $this->openAiService->generatePromptFromImage($image);
+
+        $imageGeneration = $user->imageGenerations()
+        ->create([
+            'image_path'        => $imagePath,
+            'generated_prompt'  => $generatedPrompt,
+            'original_filename' => $originalName,
+            'file_size'         => $image->getSize(),
+            'mime_type'         => $image->getMimeType(),
+        ]);
+
+        return new ImageGenerationResource($imageGeneration);
+    }
 }
