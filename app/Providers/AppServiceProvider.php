74418ca731c731a9893a7628539547931ebead1c
codeurient <codeurient@gmail.com>
2026-01-21 08:26:01 +0400

    afterOpenApiGenerated

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,11 +2,14 @@
 
 namespace App\Providers;
 
+use Dedoc\Scramble\Scramble;
 use Illuminate\Http\Request;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Support\Facades\RateLimiter;
+use Dedoc\Scramble\Support\Generator\OpenApi;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Dedoc\Scramble\Support\Generator\SecurityScheme;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +22,12 @@ class AppServiceProvider extends ServiceProvider
     {
         RateLimiter::for('api', function(Request $request) {
             return Limit::perMinute(1)->by($request->user()?->id ?: $request->ip());
+        });
+
+        Scramble::afterOpenApiGenerated(function (OpenApi $openApi) {
+            $openApi->secure(
+                SecurityScheme::http('bearer', 'BearerAuth')
+            );
         });
     }
 }
